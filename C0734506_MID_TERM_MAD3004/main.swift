84b1c6752750490d3ab2098c58ae72375901e747
MacStudent <macstudent@228-imac19.local>
2018-09-13 18:00:38 -0400

    Initial Commit

---
+++ C0734506_MID_TERM_MAD3004/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  C0734506_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2018-09-13.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
